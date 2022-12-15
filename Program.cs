00832ac3722bbe1ddc563f9ffa913ce1e022e1be
JJandke <johannesjandke@gmail.com>
2022-12-15 15:23:09 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,42 @@ namespace PaketZuordnung
     {
         static void Main(string[] args)
         {
+            int i_paketnummer = 0;
+            int i_fach = 0;
+
+            Console.Write("Bitte geben Sie die Paketnummer an: ");
+            try
+            {
+                i_paketnummer = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            if (i_paketnummer %4 == 0 || i_paketnummer % 5 == 0){
+                if (i_paketnummer % 100 == 0) {
+                    if (i_paketnummer % 400 == 0)
+                    {
+                        i_fach = 3;
+                    }
+                    else
+                    {
+                        i_fach = 2;
+                    }
+                }
+                else
+                {
+                    i_fach = 1;
+                }
+            }
+            else
+            {
+                i_fach = 4;
+            }
+            Console.WriteLine("Das Paket ist in Fach Nr. " + i_fach);
+            Console.ReadKey();
         }
     }
 }
